54928590817d2e2a3275e2c7579065932cf44768
Priyanshu Joshi <priyanshujoshi438@gmail.com>
2026-07-03 02:59:47 +0530

    Move 0's to End

--- Q1.cpp
+++ Q1.cpp
@@ -11,10 +11,23 @@ int main()
     {
         cin >> arr[i];
     }
+    int j = 0;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] == 0)
+        if (arr[i] != 0)
         {
+            arr[j] = arr[i];
+            j++;
         }
     }
+    while (j < n)
+    {
+        arr[j] = 0;
+        j++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
